facc3411872b632ded037c7e66ea4fa2b9af1e37
maratang <ginko020909@gmail.com>
2024-02-11 23:53:54 +0900

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <header>
-    <div>
-      <h1>페이지 이동 테스트</h1>
-      <nav>
-        <router-link to="/home"><b-button variant="success">home로 이동</b-button></router-link>
-        <br>
-        <router-link to="/testcomponent"><b-button variant="outline-primary">testcomponent로 이동</b-button></router-link>
-        <br>
-        <b-button @click="toggleDarkMode">DarkMode 토글</b-button>
-      </nav>
-      <br><br><br><br>
+   <div>
+    <!-- Navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header navbar-left">
+          <router-link to="/home" class="btn btn-default">홈</router-link>
+          <router-link to="/testcomponent" class="btn btn-default">Test Component</router-link>
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#" @click="toggleDarkMode">DarkMode 토글</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- 구분선 -->
+    <div class="separator"></div>
+
+    <!-- Router View -->
+    <div class="container" style="margin-top: 70px;">
+      <router-view></router-view>
     </div>
-  </header>
-  <!-- router의 url에 따라 갱신되는 영역 -->
-  <RouterView />
-
-<div>
-  <br><br><br><br><br>
-</div>
-
+  </div>
 </template>
+
 
 <script>
 import DarkMode from './darkmode.js';
@@ -46,5 +48,44 @@ export default {
 </script>
 
 <style>
+  /* 네비게이션 바 배경색 */
+  .navbar-default {
+    background-color: rgb(218, 215, 255); /* 원하는 색상으로 설정 */
+  }
+  .dark-mode .navbar-default {
+    background-color: rgb(33, 19, 36);
+  }
 
+  /* 기본 글자색 */
+  .navbar-default .navbar-brand,
+  .navbar-default .navbar-nav > li > a {  
+    color: #333;
+  }
+
+  /* DarkMode 활성화 시 글자색 변경 */
+  .dark-mode .navbar-default .navbar-brand,
+  .dark-mode .navbar-default .navbar-nav > li > a { 
+    color: #fff;
+  }
+  /* 구분선 스타일 */
+  .separator {
+    border-top: 5px solid #333; /* 기본 구분선 색상 */
+  }
+
+  /* DarkMode 활성화 시 구분선 색상 변경 */
+  .dark-mode .separator {
+    border-top-color: #fff; /* DarkMode 활성화 시 구분선 색상 */
+  }
+
+  /*버튼 스타일*/
+  .btn {
+  background-color: #333; /* 버튼의 기본 배경색 (밝은 색) */
+  color: #fff; /* 버튼의 기본 글자색 (어두운 색) */
+}
+
+/* DarkMode 활성화 시 버튼 색상 변경 */
+.dark-mode .btn {
+  background-color: #fff; /* DarkMode 활성화 시 버튼의 배경색 변경 (어두운 색) */
+  color: #333; /* DarkMode 활성화 시 버튼의 글자색 변경 (밝은 색) */
+}
 </style>
--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,6 +1,13 @@
 <template>
-  <p>
-    테스트뷰임!!!!
-  </p>
-  </template>
-  
+  <div>
+    <h1>Hi</h1>
+    <p>
+      This is Test Component!
+      몰?루
+    </p>
+  </div>
+</template>
+
+<script>
+
+</script>
